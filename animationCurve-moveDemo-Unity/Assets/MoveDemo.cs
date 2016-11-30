c6839f3232908d046506f7e39228c4bd9c100b88
Lei Wang <ahleiwolong@163.com>
2016-11-30 14:42:45 +0800

    initial

---
+++ animationCurve-moveDemo-Unity/Assets/MoveDemo.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveDemo : MonoBehaviour {
+    
+    [SerializeField] MoveItem MyMoveItem;
+    [SerializeField] AnimationCurve CurveScale,CurveY,CurveZ;
+
+    private const float INIT_POS_X = -1f;
+    private const float DISTANCE_HEIGHT = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+
+    [ContextMenu("Init")]
+    void Init() {
+        MyMoveItem.gameObject.SetActive(false);
+        NGUITools.DestroyChildren(transform);
+        for (int i=0;i<5;++i) {
+            var child = NGUITools.AddChild(gameObject, MyMoveItem.gameObject);
+            child.SetActive(true);
+            child.GetComponent<MoveItem>().SetData(i);
+            var posX = INIT_POS_X + i * DISTANCE_HEIGHT;
+            var pos = child.transform.position;
+            child.name = "Move Item "+i;
+            child.transform.position = new Vector3(posX, CurveY.Evaluate(posX), pos.z);
+            child.transform.localScale = Vector3.one * CurveScale.Evaluate(posX);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ animationCurve-moveDemo-Unity/Assets/MoveItem.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class MoveItem : MonoBehaviour
+{
+    [SerializeField]
+    UILabel NameText;
+    public void SetData(int i) {
+        NameText.text = i.ToString();
+    }
+}
